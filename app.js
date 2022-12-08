e8701bb121d16393304d66b938a13aceb8f463ae
Jeremy Guillen <guillenjeremy26@gmail.com>
2022-12-07 22:32:45 -0600

    adding timezone feature

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const cupertinoTag = document.getElementById('cupertino');
-const newYorkTag = document.getElementById('new_york_city');
-const londonTag = document.getElementById('london');
-const amsterdamTag = document.getElementById('amsterdam');
-const tokyoTag = document.getElementById('tokyo');
-const hongKongTag = document.getElementById('hong_kong');
-const sydneyTag = document.getElementById('sydney');
+let cities = [];
 let selectedItem;
+const cityNavElements = document.getElementById('cities');
+const currentTimeElement = document.getElementById('current-time');
 
+const getCityCurrentTime = async (timeZone) => {
+    try {
+        const response = await fetch(`https://worldtimeapi.org/api/timezone/${timeZone}`);
+        const data = await response.json();
+        const date = new Date(data.datetime).toLocaleString('en-US', {
+            timeZone: timeZone
+        });
+        return date;
+    } catch (e) {
+        console.error(e);
+        return new Date().toLocaleString();
+    }
+}
 
 const setSliderProperties = (width, leftOffset) => {
     document.documentElement.style.setProperty('--slide-left-offset', `${leftOffset}px`);
@@ -23,49 +32,57 @@ const setSelectedItem = (item) => {
     item.classList.add('list__city--selected');
 };
 
-const onWindowResize = (e) => {
+const onWindowResize = () => {
     if (!selectedItem) return;
     setSliderProperties(selectedItem.offsetWidth, selectedItem.offsetLeft);
 };
 
+const getCitiesData = async () => {
+   const response = await fetch('./navigation.json');
+   const data = await response.json();
+   cities = data.cities;
+};
+
+const setCurerntTimeText = (currentTime) => {
+    currentTimeElement.innerText = currentTime;
+}
+
+const createNavElements = () => {
+    if (!cities.length) return;
+    const elements = cities.map((city) => {
+        const cityElement = document.createElement('li');
+        cityElement.classList.add('list__city');
+        cityElement.id = city.section;
+        cityElement.innerText = city.label;
+        cityElement.addEventListener('click', async () => {
+            removeSelectedItem();
+            setSliderProperties(cityElement.offsetWidth, cityElement.offsetLeft);
+            setSelectedItem(cityElement);
+            const date = await getCityCurrentTime(city.time_zone);
+            setCurerntTimeText(date);
+
+        });
+        cityNavElements.appendChild(cityElement);
+        return cityElement
+    });
+    const firstElement = elements[0];
+    setSelectedItem(firstElement);
+    setSliderProperties(firstElement.offsetWidth, firstElement.offsetLeft);
+};
+
+const loadDefaultTimezone = async () => {
+    if (!cities.length) return;
+    const date = await getCityCurrentTime(cities[0].time_zone);
+    setCurerntTimeText(date);
+}
+
+
 window.addEventListener('resize', onWindowResize)
 
-cupertinoTag.addEventListener('click', () => {
-    removeSelectedItem();
-    setSliderProperties(cupertinoTag.offsetWidth, cupertinoTag.offsetLeft);
-    setSelectedItem(cupertinoTag);
-});
-newYorkTag.addEventListener('click', () => {
-    removeSelectedItem();
-    setSliderProperties(newYorkTag.offsetWidth, newYorkTag.offsetLeft);
-    setSelectedItem(newYorkTag);
-});
-londonTag.addEventListener('click', () => {
-    removeSelectedItem();
-    setSliderProperties(londonTag.offsetWidth, londonTag.offsetLeft);
-    setSelectedItem(londonTag);
-});
-amsterdamTag.addEventListener('click', () => {
-    removeSelectedItem();
-    setSliderProperties(amsterdamTag.offsetWidth, amsterdamTag.offsetLeft);
-    setSelectedItem(amsterdamTag);
-});
-tokyoTag.addEventListener('click', () => {
-    removeSelectedItem();
-    setSliderProperties(tokyoTag.offsetWidth, tokyoTag.offsetLeft);
-    setSelectedItem(tokyoTag);
+const loadNavElements = async () => {
+    await getCitiesData();
+    createNavElements();
+    loadDefaultTimezone();
+} 
 
-});
-hongKongTag.addEventListener('click', () => {
-    removeSelectedItem();
-    setSliderProperties(hongKongTag.offsetWidth, hongKongTag.offsetLeft);
-    setSelectedItem(hongKongTag);
-});
-sydneyTag.addEventListener('click', () => {
-    removeSelectedItem();
-    setSliderProperties(sydneyTag.offsetWidth, sydneyTag.offsetLeft);
-    setSelectedItem(sydneyTag);
-});
-
-setSliderProperties(cupertinoTag.offsetWidth, cupertinoTag.offsetLeft);
-setSelectedItem(cupertinoTag);
+loadNavElements();
